bfff1a63e6f0d19d2a4070d6fd3c212c271a4930
Madhuu5302 <78905649+Madhuu5302@users.noreply.github.com>
2023-12-21 09:52:52 +0530

    Add files via upload

---
+++ CASESTUDY(ARTGALLERY).sql
@@ -0,0 +1,98 @@
+CREATE DATABASE ARTGALLERY;
+USE ARTGALLERY;
+
+-- Artist Table
+CREATE TABLE Artist (
+    ArtistID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Biography TEXT,
+    BirthDate DATE,
+    Nationality VARCHAR(50),
+    Website VARCHAR(255),
+    Contact_Information VARCHAR(255)
+);
+
+-- Artwork Table
+CREATE TABLE Artwork (
+    ArtworkID INT PRIMARY KEY,
+    Title VARCHAR(255),
+    Description TEXT,
+    CreationDate DATE,
+    Medium VARCHAR(100),
+    ImageURL VARCHAR(255)
+);
+-- User Table
+CREATE TABLE Users (
+    UserID INT PRIMARY KEY,
+    Username VARCHAR(50),
+    Password VARCHAR(255),
+    Email VARCHAR(255),
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    ProfilePicture VARCHAR(255),
+    FavoriteArtworks VARCHAR(255)
+);
+
+-- Gallery Table
+CREATE TABLE Gallery (
+    GalleryID INT PRIMARY KEY,
+    Name VARCHAR(255),
+    Description TEXT,
+    Location VARCHAR(255),
+    Curator INT,
+    OpeningHours VARCHAR(255),
+    FOREIGN KEY (Curator) REFERENCES Artist(ArtistID)
+);
+
+INSERT INTO Artist VALUES
+(1, 'Vincent van Gogh', 'Post-Impressionist painter', '1853-03-30', 'Dutch', 'http://www.vangogh.com', 'info@vangogh.com'),
+(2, 'Leonardo da Vinci', 'Renaissance polymath', '1452-04-15', 'Italian', 'http://www.leonardodavinci.com', 'info@leonardo.com'),
+(3, 'Frida Kahlo', 'Surrealist painter', '1907-07-06', 'Mexican', 'http://www.fridakahlo.com', 'info@frida.com'),
+(4, 'Pablo Picasso', 'Cubist artist', '1881-10-25', 'Spanish', 'http://www.picasso.com', 'info@picasso.com'),
+(5, 'Georgia Keeffe', 'Modernist painter', '1887-11-15', 'American', 'http://www.okeeffe.com', 'info@okeeffe.com'),
+(6, 'Claude Monet', 'Impressionist painter', '1840-11-14', 'French', 'http://www.monet.com', 'info@monet.com'),
+(7, 'Salvador Dalí', 'Surrealist artist', '1904-05-11', 'Spanish', 'http://www.dali.com', 'info@dali.com'),
+(8, 'Edvard Munch', 'Symbolist painter', '1863-12-12', 'Norwegian', 'http://www.munch.com', 'info@munch.com'),
+(9, 'Mary Cassatt', 'Impressionist painter', '1844-05-22', 'American', 'http://www.cassatt.com', 'info@cassatt.com'),
+(10, 'Katsushika Hokusai', 'Edo period ukiyo-e painter', '1760-10-31', 'Japanese', 'http://www.hokusai.com', 'info@hokusai.com');
+SELECT * FROM Artist;
+
+INSERT INTO Artwork VALUES
+(1, 'Starry Night', 'A mesmerizing night sky with swirling stars', '1889-06-30', 'Oil on canvas', 'http://imageurl.com/starrynight'),
+(2, 'Mona Lisa', 'Iconic portrait with a mysterious smile', '1503-04-15', 'Oil on poplar panel', 'http://imageurl.com/monalisa'),
+(3, 'The Two Fridas', 'A symbolic representation of Kahlo identity', '1939-07-06', 'Oil on canvas', 'http://imageurl.com/twofridas'),
+(4, 'Guernica', 'Powerful anti-war painting', '1937-04-26', 'Oil on canvas', 'http://imageurl.com/guernica'),
+(5, 'Red Poppy No. VI', 'Close-up of a vibrant red poppy flower', '1928-10-25', 'Oil on canvas', 'http://imageurl.com/redpoppy'),
+(6, 'Water Lilies', 'Series of impressionist water lily paintings', '1916-11-14', 'Oil on canvas', 'http://imageurl.com/waterlilies'),
+(7, 'The Persistence of Memory', 'Melting clocks in a surreal landscape', '1931-05-11', 'Oil on canvas', 'http://imageurl.com/persistenceofmemory'),
+(8, 'The Scream', 'Expressionist depiction of anxiety', '1893-12-12', 'Oil, pastel, and casein on cardboard', 'http://imageurl.com/thescream'),
+(9, 'The Childs Bath', 'Intimate mother-child scene', '1891-05-22', 'Oil on canvas', 'http://imageurl.com/childsbath'),
+(10, 'The Great Wave off Kanagawa', 'Iconic ukiyo-e print', '1831-10-31', 'Woodblock print', 'http://imageurl.com/greatwave');
+SELECT * FROM Artwork;
+
+INSERT INTO Users VALUES
+(1, 'artlover123', 'password123', 'artlover@example.com', 'Alice', 'Johnson', '1990-12-05', 'http://profilepic1.com', '1,2,3,4,5'),
+(2, 'creative_soul', 'pass456', 'creativesoul@gmail.com', 'Bob', 'Smith', '1988-08-18', 'http://profilepic2.com', '6,7,8,9,10'),
+(3, 'gallery_owner', 'pass789', 'galleryowner@gmail.com', 'Catherine', 'Miller', '1975-03-22', 'http://profilepic3.com', '2,5,7,9,10'),
+(4, 'modern_artist', 'passArt', 'modernartist@gmail.com', 'David', 'White', '1983-07-10', 'http://profilepic4.com', '1,3,6,8,9'),
+(5, 'impressionist_fan', 'passImp', 'impressionistfan@gmail.com', 'Emily', 'Clark', '1995-01-28', 'http://profilepic5.com', '2,6,7,8,10'),
+(6, 'art_collector', 'passCollect', 'artcollector@gmail.com', 'Frank', 'Taylor', '1960-09-14', 'http://profilepic6.com', '1,4,5,7,10'),
+(7, 'student_artist', 'passArtStudent', 'artstudent@gmail.com', 'Grace', 'Young', '2000-04-02', 'http://profilepic7.com', '3,5,6,8,9'),
+(8, 'abstract_lover', 'passAbstract', 'abstractlover@gmail.com', 'Henry', 'Brown', '1989-11-20', 'http://profilepic8.com', '1,2,4,7,10'),
+(9, 'classic_art_enthusiast', 'passClassic', 'classicart@gmail.com', 'Isabel', 'Lee', '1972-06-08', 'http://profilepic9.com', '3,4,6,8,9'),
+(10, 'digital_artist', 'passDigital', 'digitalartist@gmail.com', 'Jake', 'Martin', '1993-02-15', 'http://profilepic10.com', '1,2,5,7,9');
+SELECT * FROM Users;
+
+INSERT INTO Gallery VALUES
+(1, 'Modern Art Hub', 'Showcasing contemporary artwork', '123 Main St, Cityville', 1, 'Mon-Fri: 10 AM - 6 PM, Sat: 12 PM - 4 PM'),
+(2, 'Classic Impressions Gallery', 'A collection of timeless masterpieces', '456 Art Avenue, Townsville', 2, 'Tue-Sun: 11 AM - 7 PM'),
+(3, 'Abstract Expression', 'Exploring the world of abstract art', '789 Abstract Lane, Artropolis', 4, 'Wed-Sat: 1 PM - 8 PM, Sun: 11 AM - 4 PM'),
+(4, 'Impressionist Delight', 'Journey through impressionist landscapes', '567 Artful Street, Paintingtown', 6, 'Thu-Fri: 12 PM - 6 PM, Sat: 10 AM - 5 PM'),
+(5, 'Surreal Dreams Gallery', 'A surrealistic experience for art enthusiasts', '890 Dreamy Blvd, Visionville', 7, 'Tue-Sun: 10 AM - 5 PM'),
+(6, 'Nature Canvas Gallery', 'Celebrating art inspired by nature', '234 Greenery Lane, Earthville', 9, 'Mon-Sat: 11 AM - 7 PM, Sun: 12 PM - 5 PM'),
+(7, 'Digital Wonders Exhibit', 'Exploring the digital realm of artistic creations', '345 Tech Street, Digicity', 10, 'Wed-Fri: 2 PM - 9 PM, Sat-Sun: 10 AM - 6 PM'),
+(8, 'Timeless Classics Gallery', 'Rediscover the beauty of classical art', '678 Elegance Avenue, Classicburg', 3, 'Thu-Sun: 12 PM - 8 PM'),
+(9, 'Gallery of Colors', 'Vibrant showcase of colorful masterpieces', '901 Rainbow Road, Chromacity', 5, 'Tue-Sat: 11 AM - 6 PM, Sun: 1 PM - 5 PM'),
+(10, 'Future Visions Gallery', 'Dive into the future through visionary art', '432 Futuristic Lane, Tomorrowtown', 8, 'Mon-Fri: 1 PM - 7 PM, Sat: 11 AM - 4 PM');
+SELECT * FROM Gallery;
---
+++ CASESTUDY(CRIMEREPORT).sql
@@ -0,0 +1,167 @@
+CREATE DATABASE CRIMEREPORT;
+USE CRIMEREPORT;
+-- Victims Table
+CREATE TABLE Victims (
+    VictimID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Gender VARCHAR(10),
+    ContactInformation VARCHAR(255)
+);
+
+-- Suspects Table
+CREATE TABLE Suspects (
+    SuspectID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    DateOfBirth DATE,
+    Gender VARCHAR(10),
+    ContactInformation VARCHAR(255) -- Assuming Address and Phone Number
+);
+
+-- Incidents Table
+CREATE TABLE Incidents (
+    IncidentID INT PRIMARY KEY,
+    IncidentType VARCHAR(50),
+    IncidentDate DATE,
+    Location GEOGRAPHY, -- Assuming geospatial data (Latitude and Longitude)
+    Description VARCHAR(255),
+    Status VARCHAR(20),
+    VictimID INT,
+    SuspectID INT,
+    FOREIGN KEY (VictimID) REFERENCES Victims(VictimID),
+    FOREIGN KEY (SuspectID) REFERENCES Suspects(SuspectID)
+);
+
+-- Law Enforcement Agencies Table
+CREATE TABLE LawEnforcementAgencies (
+    AgencyID INT PRIMARY KEY,
+    AgencyName VARCHAR(100),
+    Jurisdiction VARCHAR(100),
+    ContactInformation VARCHAR(255) -- Assuming Address and Phone Number
+);
+
+-- Officers Table
+CREATE TABLE Officers (
+    OfficerID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    BadgeNumber VARCHAR(20),
+    Rank VARCHAR(50),
+    ContactInformation VARCHAR(255), -- Assuming Address and Phone Number
+    AgencyID INT,
+    FOREIGN KEY (AgencyID) REFERENCES LawEnforcementAgencies(AgencyID)
+);
+
+-- Evidence Table
+CREATE TABLE Evidence (
+    EvidenceID INT PRIMARY KEY,
+    Description TEXT,
+    LocationFound VARCHAR(255),
+    IncidentID INT,
+    FOREIGN KEY (IncidentID) REFERENCES Incidents(IncidentID)
+);
+
+-- Reports Table
+CREATE TABLE Reports (
+    ReportID INT PRIMARY KEY,
+    IncidentID INT,
+    ReportingOfficer INT,
+    ReportDate DATE,
+    ReportDetails TEXT,
+    Status VARCHAR(20),
+    FOREIGN KEY (IncidentID) REFERENCES Incidents(IncidentID),
+    FOREIGN KEY (ReportingOfficer) REFERENCES Officers(OfficerID)
+);
+
+INSERT INTO Victims VALUES
+(1, 'Alice', 'Johnson', '1990-12-05', 'Female', '123 Main St, Cityville, 9870312345'),
+(2, 'Bob', 'Smith', '1988-08-18', 'Male', '456 Oak Ave, Townsville, 9873167890'),
+(3, 'Catherine', 'Miller', '1975-03-22', 'Female', '789 Elm St, Villagetown, 8790745678'),
+(4, 'David', 'White', '1983-07-10', 'Male', '101 Pine Ln, Countryside, 7690878901'),
+(5, 'Emily', 'Clark', '1995-01-28', 'Female', '202 Cedar Rd, Natureville, 9980723456'),
+(6, 'Frank', 'Taylor', '1960-09-14', 'Male', '303 Maple Dr, Urbancity, 9914534567'),
+(7, 'Grace', 'Young', '2000-04-02', 'Female', '404 Birch Blvd, Digitaltown, 9003245678'),
+(8, 'Henry', 'Brown', '1989-11-20', 'Male', '505 Redwood St, Classicburg, 8715656789'),
+(9, 'Isabel', 'Lee', '1972-06-08', 'Female', '606 Walnut Ave, Chromacity, 86670967890'),
+(10, 'Jake', 'Martin', '1993-02-15', 'Male', '707 Spruce Ln, Tomorrowtown, 9023578901');
+SELECT * FROM Victims;
+
+INSERT INTO Suspects VALUES
+(101, 'John', 'Doe', '1985-06-20', 'Male', '456 Suspicion St, Shadytown, 9065412345'),
+(102, 'Jane', 'Smith', '1990-03-15', 'Female', '789 Guilty Ave, Crimetown, 86670967890'),
+(103, 'Mike', 'Johnson', '1978-09-02', 'Male', '101 Fraud St, Scamville, 9807623456'),
+(104, 'Sara', 'Williams', '1982-11-28', 'Female', '202 Burglar Blvd, Breakinburg, 9321434567'),
+(105, 'Alex', 'Davis', '1995-07-14', 'Male', '303 Narcotics Dr, Drugsville, 9543145678'),
+(106, 'Laura', 'Miller', '1988-05-06', 'Female', '404 Vandalism Ave, Smashville, 8871656789'),
+(107, 'Chris', 'Brown', '1992-01-10', 'Male', '505 Kidnap Ln, Hostagetown, 9112867890'),
+(108, 'Amanda', 'White', '1980-04-18', 'Female', '606 Forgery Rd, Faketown, 9223978901'),
+(109, 'Tom', 'Jackson', '1976-12-22', 'Male', '707 Assault St, Violentown, 9733789012'),
+(110, 'Rachel', 'Anderson', '1991-08-30', 'Female', '808 Harassment Blvd, Disturbia, 7339390123');
+SELECT * FROM Suspects;
+
+INSERT INTO Incidents VALUES
+(1, 'Robbery', '2023-01-15', geography::Point(37.7749, -122.4194, 4326), 'Armed robbery at a convenience store', 'Open', 1, 102),
+(2, 'Homicide', '2023-02-05', geography::Point(34.0522, -118.2437, 4326), 'Investigating a murder case in downtown', 'Under Investigation', 3, 104),
+(3, 'Theft', '2023-03-10', geography::Point(40.7128, -74.0060, 4326), 'Report of stolen property from a residence', 'Closed', 5, 106),
+(4, 'Assault', '2023-04-20', geography::Point(41.8781, -87.6298, 4326), 'Physical assault in a public park', 'Open', 7, 108),
+(5, 'Burglary', '2023-05-12', geography::Point(51.5074, -0.1278, 4326), 'Break-in reported at a residential property', 'Open', 9, 110),
+(6, 'Fraud', '2023-06-25', geography::Point(45.4215, -75.6993, 4326), 'Investigating a case of financial fraud', 'Under Investigation', 2, 103),
+(7, 'Kidnapping', '2023-07-08', geography::Point(33.7490, -84.3880, 4326), 'Report of a kidnapping incident', 'Open', 4, 105),
+(8, 'Vandalism', '2023-08-14', geography::Point(37.7749, -122.4194, 4326), 'Incident of property vandalism', 'Closed', 6, 107),
+(9, 'Drug Offense', '2023-09-03', geography::Point(40.7128, -74.0060, 4326), 'Investigation into drug-related activities', 'Open', 8, 109),
+(10, 'Domestic Violence', '2023-10-19', geography::Point(34.0522, -118.2437, 4326), 'Responding to a domestic violence case', 'Under Investigation', 10, 101);
+SELECT * FROM Incidents;
+
+INSERT INTO LawEnforcementAgencies VALUES
+(11, 'Cityville Police Department', 'Cityville Jurisdiction', '123 Main St, Cityville, 1234567890'),
+(12, 'Townsville Sheriff Office', 'Townsville Jurisdiction', '456 Oak Ave, Townsville, 7854367890'),
+(13, 'Villagetown Police Department', 'Villagetown Jurisdiction', '789 Elm St, Villagetown, 68482845678'),
+(14, 'Countryside Police Department', 'Countryside Jurisdiction', '101 Pine Ln, Countryside, 84586578901'),
+(15, 'Natureville Rangers', 'Natureville Jurisdiction', '202 Cedar Rd, Natureville, 43855423456'),
+(16, 'Urbancity Law Enforcement', 'Urbancity Jurisdiction', '303 Maple Dr, Urbancity, 83478534567'),
+(17, 'Digitaltown Security', 'Digitaltown Jurisdiction', '404 Birch Blvd, Digitaltown, 73487545678'),
+(18, 'Classicburg Police Department', 'Classicburg Jurisdiction', '505 Redwood St, Classicburg, 324768756789'),
+(19, 'Chromacity Police Department', 'Chromacity Jurisdiction', '606 Walnut Ave, Chromacity, 78437867890'),
+(20, 'Tomorrowtown Security', 'Tomorrowtown Jurisdiction', '707 Spruce Ln, Tomorrowtown, 723784378901');
+SELECT * FROM LawEnforcementAgencies;
+
+INSERT INTO Officers VALUES
+(201, 'Mark', 'Johnson', '12345', 'Chief', '123 Main St, Cityville, 12345', 11),
+(202, 'Linda', 'Smith', '67890', 'Sheriff', '456 Oak Ave, Townsville, 67890', 12),
+(203, 'Robert', 'White', '45678', 'Chief', '789 Elm St, Villagetown, 45678', 13),
+(204, 'Sarah', 'Davis', '78901', 'Chief', '101 Pine Ln, Countryside, 78901', 14),
+(205, 'Alex', 'Green', '23456', 'Ranger', '202 Cedar Rd, Natureville, 23456', 15),
+(206, 'Laura', 'Miller', '34567', 'Commissioner', '303 Maple Dr, Urbancity, 34567', 16),
+(207, 'Sam', 'White', '45678', 'Director', '404 Birch Blvd, Digitaltown, 45678', 17),
+(208, 'Monica', 'Brown', '56789', 'Chief', '505 Redwood St, Classicburg, 56789', 18),
+(209, 'Andrew', 'Jackson', '67890', 'Chief', '606 Walnut Ave, Chromacity, 67890', 19),
+(210, 'Jessica', 'Anderson', '78901', 'Director', '707 Spruce Ln, Tomorrowtown, 78901', 20);
+SELECT * FROM Officers;
+
+INSERT INTO Evidence VALUES
+(301, 'Security footage from convenience store', '123 Main St, Cityville', 1),
+(302, 'DNA samples found at crime scene', '456 Oak Ave, Townsville', 2),
+(303, 'Stolen items recovered from suspect', '789 Elm St, Villagetown', 3),
+(304, 'Witness statements and sketches', '101 Pine Ln, Countryside', 4),
+(305, 'Fingerprints on broken window', '202 Cedar Rd, Natureville', 5),
+(306, 'Financial transaction records', '303 Maple Dr, Urbancity', 6),
+(307, 'Video evidence from security cameras', '404 Birch Blvd, Digitaltown', 7),
+(308, 'Vandalized property photographs', '505 Redwood St, Classicburg', 8),
+(309, 'Drug paraphernalia and substances', '606 Walnut Ave, Chromacity', 9),
+(310, 'Domestic violence incident report', '707 Spruce Ln, Tomorrowtown', 10);
+SELECT * FROM Evidence;
+
+INSERT INTO Reports VALUES
+(401, 1, 201, '2023-01-16', 'Initial incident report for robbery', 'Draft'),
+(402, 2, 202, '2023-02-06', 'Ongoing investigation report for homicide', 'Draft'),
+(403, 3, 203, '2023-03-11', 'Finalized report for theft case', 'Finalized'),
+(404, 4, 204, '2023-04-21', 'Incident report for assault in public park', 'Draft'),
+(405, 5, 205, '2023-05-13', 'Progress report for burglary investigation', 'Draft'),
+(406, 6, 206, '2023-06-26', 'Financial fraud case report', 'Draft'),
+(407, 7, 207, '2023-07-09', 'Kidnapping incident report', 'Draft'),
+(408, 8, 208, '2023-08-15', 'Finalized report for vandalism case', 'Finalized'),
+(409, 9, 209, '2023-09-04', 'Drug offense investigation report', 'Draft'),
+(410, 10, 210, '2023-10-20', 'Domestic violence incident report', 'Draft');
+SELECT * FROM Reports;
